5053223c90986ebe4b1667677887f8b70deba26b
Calvin-Muchemwa <calvinmuchemwa123@gmail.com>
2021-11-28 20:49:41 +0200

    finished CRUD operations(1)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI ,Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -14,6 +14,18 @@ class Post(BaseModel):## we use this class to define how our post properties (po
     published:bool=True
     rating:Optional[int]=None
 
+
+def find_post(id):
+    for post in my_posts:
+        if post['id']==id:
+            return post
+
+def find_post_index(id):
+    for i , post in enumerate(my_posts):
+        if post["id"]==id:
+            return i
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello Worlds"}
@@ -22,21 +34,36 @@ def read_root():
 def get_posts():
     return{"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):##whatever data we giving api will be converted to python dict Body
     post_dict=post.dict();
     post_dict['id']=randrange(0,10000000)
     my_posts.append(post_dict);
-    ##easily conver pydantic object to dict and add to our array
+    ##easily convert pydantic object to dict and add to our array
     return{"message":"Successfully added"} ##extracting data
 
 
 @app.get("/posts/{id}")##This time we getting the path parameter id in order to retrieve the post
-def get_post(id):
-    for post in my_posts:
-        if post['id']==2:
-            print(post)
-
-    return{"message":f"Printing out post with id : {id}"}
+def get_post(id:int,response:Response):###id:int automatically validate it to int
+    post=find_post(id)
+    if not post:
+        raise HTTPException(status_code=404,detail=f"post with {id} does not exist")##raise error as exception, specify status code and detail message
+       
+    print(post)
+    return{"post_detail":post}
     
-        
+            
+@app.delete("/posts/{id}",status_code=204)
+def delete_post(id:int):
+    i=find_post_index(id)
+    if not i:
+        raise HTTPException(status_code=404,detail=f"post with {id} does not exist")
+    my_posts.pop(i)
+    return{"message":f"post with ID: {id} has successfully been deleted"}
+
+@app.put("/posts/{id}",status_code=status.HTTP_201_CREATED)##put request when updating post
+def update_post(id:int,post:Post):
+    i=find_post_index(id)
+    post_dict=post.dict()
+    my_posts[i]=post_dict
+    return{"message":"Post successfully updated"}
